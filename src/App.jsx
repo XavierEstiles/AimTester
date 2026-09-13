911f9ccb2a03537dc70838001e9d02f682ac6feb
XavierEstiles <Xavistyles_beltran@hotmail.com>
2026-09-13 21:10:12 +0200

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,47 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [position, setPosition] = useState({ x: 490, y: 240 })
+  const [puntos, setPuntos] = useState({x:0})
+  const [fallos, setFallos] = useState({x:0})
+
+  
+
+  function puntuar(e){
+    moverCuadrado();
+    setPuntos({x: puntos.x+1});
+  }
 
   function moverCuadrado() {
     setPosition({
-      x:  Math.random() * 990,
-      y: Math.random() * 490
+      x:  Math.random() * 950,
+      y: Math.random() * 450
     })
   }
 
+  function fallar(e){
+     if (e.target === e.currentTarget) {
+      setFallos({x: fallos.x+1})
+    }
+  }
+
+  function calcularAciertos(){
+    return puntos.x+fallos.x>0?(puntos.x/(puntos.x+fallos.x)*100).toFixed(0):0
+  }
 
   return (
     <div className="app">
-      <h1>Atrapa el cuadrado</h1>
-      <div className="tablero">
-        <div className="cuadrado" style={{ position: 'absolute', left: position.x, top: position.y }} onClick={moverCuadrado}></div>
+      <h1>Aim Tester</h1>
+      <div className="informacion">
+        <p>Puntos: {puntos.x}</p>
+        <p>Fallos: {fallos.x}</p>
+        <p>Acierto: {calcularAciertos()}%</p>
+      </div>
+      <div className="tablero" onClick={fallar}>
+        <div className="cuadrado" style={{ position: 'absolute', left: position.x, top: position.y }} onClick={puntuar}></div>
       </div>
     </div>
   )
 }
-
-
 
 export default App
